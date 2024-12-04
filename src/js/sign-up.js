7bebed8f77d7281a37bc5575e81e4a5162d8c23f
Oleksandr Kropivnyi <kropivnyy@gmail.com>
2024-12-04 14:20:03 +0200

    opened BR sign up

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -1,13 +1,9 @@
 import handlebars from 'handlebars';
-import {
-  // SignUpForm,
-  compileSignUpFormMarkup,
-  // setToLS
-} from 'mayanbet-sdk';
+import { SignUpForm, compileSignUpFormMarkup, setToLS } from 'mayanbet-sdk';
 import signUpBonusesTemplate from '@/partials/sign-up-bonuses.hbs?raw';
 import { openModal } from '@/js/modal';
 import { globalState } from '@/js/global-state';
-// import { REDIRECT_PARAMS } from '@/const';
+import { REDIRECT_PARAMS } from '@/const';
 
 const modalContentRef = document.querySelector('.js-app-modal-content');
 
@@ -27,13 +23,13 @@ export const openSignUpModal = ({ isBlocked } = {}) => {
   modalContentRef.innerHTML = '';
   modalContentRef.insertAdjacentHTML('beforeend', markup);
 
-  // new SignUpForm({
-  //   formRef: document.forms.signUp,
-  //   submitCallback: async () => {
-  //     setToLS('isAlreadyRegistered', true);
-  //   },
-  //   redirectParams: REDIRECT_PARAMS,
-  // });
+  new SignUpForm({
+    formRef: document.forms.signUp,
+    submitCallback: async () => {
+      setToLS('isAlreadyRegistered', true);
+    },
+    redirectParams: REDIRECT_PARAMS,
+  });
 
   openModal({ isBlocked });
 };
